61aea060413b4601271bcdedb2a79914c7b4f83d
Kumary Sirikumara <it17168250@my.sliit.lk>
2020-08-18 22:40:51 +0530

    Main activity altered

--- app/src/main/java/com/example/educationloancalculator/MainActivity.java
+++ app/src/main/java/com/example/educationloancalculator/MainActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    int loan_period;
-    int loan_amount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
